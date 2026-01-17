491afdafffd76bd0a5a6646a8faa4c35940b87ed
AldinKojic <aldin.kojic1809@gmail.com>
2026-01-17 06:25:06 +0100

    Add view toggle and Pokémon sound

--- script's/script.js
+++ script's/script.js
@@ -1,6 +1,13 @@
 
 let shinyMode = false;
-let allPokemons = [];
+  
+let pokemonListUrl = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=0";
+let pokemonList = [];
+let allPokemons=[];
+let showNormalSprites = false;
+
+
+
 
 async function loadSinglePokemon(url) {
   let response = await fetch(url);
@@ -9,36 +16,73 @@ async function loadSinglePokemon(url) {
 
 async function init() {
   try {
-    let response = await fetch(
-      "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0"
-    );
+    let response = await fetch(pokemonListUrl);
     let data = await response.json();
-    for (let i = 0; i < data.results.length; i++) {
-      let pokemonUrl = data.results[i].url;
-      let pokemon = await loadSinglePokemon(pokemonUrl);
+    pokemonListUrl = data.next;
+      for (let i = 0; i < data.results.length; i++) {
+        pokemonList.push(data.results[i]);
+     } 
 
-      let container = document.getElementById(`pokemon`);
+     
+     
+       for (let i = 0; i < data.results.length; i++) {
+     let pokemonUrl = data.results[i].url;
+     let pokemon = await loadSinglePokemon(pokemonUrl);
+ allPokemons.push(pokemon);
+ console.log(pokemon);
+     let container = document.getElementById(`pokemon`);
       if (!container) continue;
 
-      container.innerHTML += renderPokemons(pokemon);
+   container.innerHTML += renderPokemons(pokemon, i);
+
     }
     const randomId = getRandomId(200);
     await showPokemonDetailById(randomId);
+
+
   } catch (error) {
     console.error(error);
   }
 }
 
-async function loadPokemonSpecies(id) {
-  let response = await fetch(
-    `https://pokeapi.co/api/v2/pokemon-species/${id}/`
-  );
-  return await response.json();
+ async function loadSinglePokemon2(){
+   for(pokemonIndex=0; pokemonIndex < pokemonList.length;pokemonIndex++ ){
+  let singlePokemon = await fetch(pokemonList[pokemonIndex].url);
+  let singlePokeonAsJson = await singlePokemon.json();
+  allPokemons.push(singlePokeonAsJson)
+   }
 }
 
+
+
+
+ async function loadPokemonSpecies(id) {
+   let response = await fetch(
+     `https://pokeapi.co/api/v2/pokemon-species/${id}/`
+);
+   return await response.json();
+ }
+
+// async function loadPokemonChain(url) {
+//   let res = await fetch(url);
+//   return await response.json();
+
+// }
+
+async function pokemonEvoChain() {
+  const chain = await loadPokemonChain(id);
+  for (let i = 0; i < chain.id.length; i++) {
+    const chainID = chain
+  }
+
+}
 function getDexText(species) {
   for (let i = 0; i < species.flavor_text_entries.length; i++) {
-    return species.flavor_text_entries[i].flavor_text.replace(/\f|\n|\r/g, " ");
+    let entry = species.flavor_text_entries[i];
+
+    if (entry.language.name === "en") {
+      return entry.flavor_text.replace(/\f|\n|\r/g, " ");
+    }
   }
 }
 
@@ -50,8 +94,10 @@ async function showPokemonDetailById(id) {
   const dexText = getDexText(species);
 
   const detail = document.getElementById("detailView");
+  
   if (!detail) return;
   detail.innerHTML = renderPokemonDetail(pokemon, dexText);
+  prepareAudioByPokemon(pokemon);
 }
 
 function getRandomId(max) {
@@ -86,4 +132,9 @@ function toggleDarkMode() {
   } else {
     icon.textContent = "🌙";
   }
+}
+
+
+function capitalizeFirstLetter(text) {
+  return text.charAt(0).toUpperCase() + text.slice(1);
 }
--- script's/template.js
+++ script's/template.js
@@ -1,9 +1,13 @@
 function renderPokemons(pokemon) {
+  let imgSrc = showNormalSprites
+    ? pokemon.sprites.front_default
+    : pokemon.sprites.other["official-artwork"].front_default;
+
   return `
     <button class="pokemon-card" onclick="showPokemonDetailById(${pokemon.id})">
-      <img class="pokemon-sprite" src="${pokemon.sprites.front_default}">
+      <img class="pokemon-sprite"  id="sprite-${pokemon.id}" src="${imgSrc}">
       <p class="pokemonID">N°${pokemon.id}</p>
-      <p class="pokemon-name-template">${pokemon.name}</p>
+      <p class="pokemon-name-template">${capitalizeFirstLetter(pokemon.name)}</p>
       <div class="types-row">
         ${pokemonTypes(pokemon)}
       </div>
@@ -34,7 +38,7 @@ function pokemonAbilities(pokemon) {
       break;
     }
     html += ` <div class="types-row">
-<span class="pokemon-ability-list"> <p class="stat-badge">${typeName}</p></span> </div>
+<span class="pokemon-ability-list"> <p class="stat-badge">${capitalizeFirstLetter(typeName)}</p></span> </div>
 `;
   }
 
@@ -70,19 +74,37 @@ function renderPokemonDetail(pokemon, dexText) {
                    </div>
                                  ${renderPokemonProfile(pokemon, dexText)}
                                  ${renderPokemonCombatStats(pokemon)}
-                    <div class="detail-section-title">Evolution</div>
+                    <div class="detail-section-title">Pokémon Sound</div>
+                    <audio id="pokemonAudio" controls></audio>
+
            </div>
        </div>
                `;
+           
 }
 
 function renderPokemonHeader(pokemon) {
+    let imgSrc = showNormalSprites
+    ? pokemon.sprites.front_default
+    : pokemon.sprites.other["official-artwork"].front_default;
+
   return `
-     <img class="pokemon-detail-sprite " src="${pokemon.sprites.front_default}">
-                  <p class="pokemon-detail-id ">N°${pokemon.id}</p>
-                  <p class="pokemon-name">${pokemon.name}</p>
-                  `;
+  <div class="sprite-center">
+    <img
+      class="pokemon-detail-sprite pixelated "
+    
+      id="detailSprite"
+      src="${imgSrc}"
+      data-artwork="${pokemon.sprites.other["official-artwork"].front_default}"
+      data-normal="${pokemon.sprites.front_default}"
+      data-shiny="${pokemon.sprites.front_shiny}"
+    >
+    <p class="pokemon-detail-id">N°${pokemon.id}</p>
+    <p class="pokemon-name">${capitalizeFirstLetter(pokemon.name)}</p>
+    </div>
+  `;
 }
+
 
 function renderPokemonProfile(pokemon, dexText) {
   return `
@@ -108,6 +130,78 @@ function renderPokemonCombatStats(pokemon) {
                       <div class="stats-wrapper">
                                 ${pokemonStats(pokemon)}
                         </div>
-    
+   
     `;
+}
+
+
+function toggleAllSprites() {
+  showNormalSprites = !showNormalSprites;
+
+  renderAllPokemons();
+
+  const detailImg = document.getElementById("detailSprite");
+  if (!detailImg) return;
+
+ 
+  detailImg.src = showNormalSprites
+    ? detailImg.dataset.normal
+    : detailImg.dataset.artwork;
+
+ 
+  if (showNormalSprites) {
+    detailImg.classList.add("pixelated");
+  } else {
+    detailImg.classList.remove("pixelated");
+  }
+}
+
+function toggleShiny() {
+  showNormalSprites = !showNormalSprites;
+
+  renderAllPokemons();
+
+  const detailImg = document.getElementById("detailSprite");
+  if (!detailImg) return;
+  detailImg.src = showNormalSprites
+    ? detailImg.dataset.artwork
+    : detailImg.dataset.shiny;
+
+
+}
+function renderAllPokemons() {
+  let container = document.getElementById("pokemon");
+  container.innerHTML = "";
+
+  for (let i = 0; i < allPokemons.length; i++) {
+    container.innerHTML += renderPokemons(allPokemons[i], i);
+  }
+}
+function prepareAudioByPokemon(pokemon) {
+  const audio = document.getElementById("pokemonAudio");
+  const detailSprite = document.getElementById("detailSprite");
+
+  
+  const listSprite = document.getElementById(`sprite-${pokemon.id}`);
+
+  if (!audio || !detailSprite) return;
+  if (!pokemon?.cries?.latest) return;
+
+  audio.src = pokemon.cries.latest;
+  audio.volume = 0.1;
+
+  audio.onplay = () => {
+    detailSprite.classList.add("shake");
+    if (listSprite) listSprite.classList.add("shake");  
+  };
+
+  audio.onpause = () => {
+    detailSprite.classList.remove("shake");
+    if (listSprite) listSprite.classList.remove("shake");
+  };
+
+  audio.onended = () => {
+    detailSprite.classList.remove("shake");
+    if (listSprite) listSprite.classList.remove("shake");
+  };
 }
